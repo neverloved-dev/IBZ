8265f9e891f20903ef1bf768ab0a4f074578d49f
SadLoneWolf <djordjeandjic99@gmail.com>
2021-03-11 13:37:46 +0100

    Paired with Sofy Fairy Tell

--- Python/Dif_Helman.py
+++ Python/Dif_Helman.py
@@ -6,19 +6,19 @@ import primeRoot
 class Dif_Helman_cypher:
     # вычисление секретного числа
     @classmethod
-    def secret_number_generator(self):
+    def secret_number_generator():
         big_secret_number = randint(999, 999999)
         return big_secret_number
 
     # вычисление открытого ключа
     @classmethod
-    def key_generator(self, value, big_secret_number, prime_root):
+    def key_generator( value, big_secret_number, prime_root):
         public_key = (prime_root**big_secret_number)%value
         return public_key
 
     # вычисление секретного ключа
     @classmethod
-    def secret_key_generator(self, public_key, big_secret_number, value):
+    def secret_key_generator(public_key, big_secret_number, value):
         secret_key = (public_key**big_secret_number)%value
         return secret_key
 
